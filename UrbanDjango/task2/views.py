aa3047a82cec1ed4d877f37cc91ee9ed2e802a09
Duxita <Duxita@mail.ru>
2024-12-14 17:55:47 +0300

    Add files via upload

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 def func(request):
     return render(request, 'second_task/func_template.html')
+
 class ClassView(TemplateView):
     template_name = 'second_task/class_template.html'
-
 
